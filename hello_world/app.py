8727103fc87a634745ea4accf4f5c6093a3a9fcf
abenben <abenbenben@gmail.com>
2021-12-31 09:10:05 +0900

    Slack通知

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,10 +1,31 @@
 import json
 import requests
+import slackweb
+import os
+
+# 環境変数取得
+# channel_list = os.environ.get("CHANNEL_LIST").split(",")
+channel_list = ["https://hooks.slack.com/services/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"]
+
+
+def post_slack(channel, msg):
+    """
+    SLackへの通知
+    :param channel:Slackのチャンネル
+    :param msg:Slackへの出力
+    """
+    slack = slackweb.Slack(url=channel)
+    slack.notify(attachments=[{"color": "info", "text": msg}])
 
 
 def lambda_handler(event, context):
     try:
         ip = requests.get("http://checkip.amazonaws.com/")
+        for channel in channel_list:
+            try:
+                post_slack(channel, "TEST")
+            except Exception as e:
+                post_slack(channel, "TEST")
     except requests.RequestException as e:
         # Send some context about this error to Lambda Logs
         print(e)
