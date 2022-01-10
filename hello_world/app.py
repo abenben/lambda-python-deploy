4cc2e67bf251babac6b606086c61ec7a9353a816
abenben <abenbenben@gmail.com>
2022-01-10 12:19:53 +0900

    修正

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,17 +1,18 @@
+"""アプリテスト1"""
 import json
 import requests
 import slackweb
-import os
 
 # 環境変数取得
-# channel_list = os.environ.get("CHANNEL_LIST").split(",")
-# channel_list = ["https://hooks.slack.com/services/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"] # STAR
-channel_list = ["https://hooks.slack.com/services/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"] # テスト用
+# channel = os.environ.get("CHANNEL_LIST").split(",")
+channel = "https://hooks.slack.com/services/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"  # STAR
+channel = "https://hooks.slack.com/services/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"  # テスト用
 
 
 def post_slack(channel, msg):
-    """
-    SLackへの通知
+    """SLack通知
+
+    Slackのチャンネルに通知する.
     :param channel:Slackのチャンネル
     :param msg:Slackへの出力
     """
@@ -20,22 +21,21 @@ def post_slack(channel, msg):
 
 
 def lambda_handler(event, context):
+    """ラムダを実行
+
+    ラムダ関数を実行する.
+    :param event:
+    :param context:
+    :return:
+    """
     try:
-        ip = requests.get("http://checkip.amazonaws.com/")
-        for channel in channel_list:
-            try:
-                post_slack(channel, "TEST")
-            except Exception as e:
-                post_slack(channel, "TEST")
+        post_slack(channel, "TEST")
+
     except requests.RequestException as e:
-        # Send some context about this error to Lambda Logs
         print(e)
         raise e
 
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            "location": ip.text.replace("\n", "")
-        }),
+        "body": json.dumps({"message": "SLack通知"}),
     }
--- hello_world/app2.py
+++ hello_world/app2.py
@@ -1,8 +1,16 @@
+"""アプリテスト2"""
 import json
 import requests
 
 
 def lambda_handler2(event, context):
+    """ラムダを実行
+
+    ラムダ関数を実行する.
+    :param event: 未使用
+    :param context: 未使用
+    :return: ステータス＆結果を返す
+    """
     try:
         ip = requests.get("http://checkip.amazonaws.com/")
     except requests.RequestException as e:
@@ -12,8 +20,7 @@ def lambda_handler2(event, context):
 
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world2",
-            "location": ip.text.replace("\n", "")
-        }),
+        "body": json.dumps(
+            {"message": "hello world2", "location": ip.text.replace("\n", "")}
+        ),
     }
